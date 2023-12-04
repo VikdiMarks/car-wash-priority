e6c8d594a63acf5d626cebf3253af3f77ced39cf
VikdiMarks <sarankov.dima@gmail.com>
2023-12-04 10:11:20 +0300

    edit icon add driver

--- app/(pages)/(platform)/drivers/_components/Item.jsx
+++ app/(pages)/(platform)/drivers/_components/Item.jsx
@@ -1,18 +1,24 @@
 "use client";
 
 import Image from "next/image";
+import clsx from "clsx";
+import { useState } from "react";
 
 export default function Item({ phone, balance, races, deleteDriver, editDriver }) {
+	const [balanceDriver, setBalanceDriver] = useState(balance);
+
 	return (
 		<div className={"flex w-full text-black-100"}>
 			<div className="flex basis-1/4 items-center px-4 lg:px-1 py-2">{phone}</div>
 			<div className="flex-middle basis-1/4 px-4 lg:px-1 py-2">
-				<div
-					className={
-						"text-green--main text-sm md:text-xs p-1 bg-white border border-black/10 rounded-lg w-full flex-middle"
-					}>
-					{balance}
-				</div>
+				<input
+					className={clsx(
+						"text-sm md:text-xs p-1 bg-white border border-black/10 rounded-lg w-full flex-middle text-center",
+						{ "text-green--main": balanceDriver > 50, "text-red-600": balanceDriver < 50 },
+					)}
+					defaultValue={balanceDriver}
+					onChange={e => setBalanceDriver(e.target.value)}
+				/>
 			</div>
 			<div className="flex-middle basis-1/4 px-4 lg:px-1 py-2">{races}</div>
 			<div className="flex-middle basis-1/4 px-4 lg:px-1 py-2 justify-evenly">
