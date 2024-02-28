ae3342fe0c2ae94ca8e053c5f03f20f1e60c9ba6
VikdiMarks <sarankov.dima@gmail.com>
2024-02-28 11:30:41 +0300

    fix

--- app/(pages)/(platform)/_components/ZeroContent.jsx
+++ app/(pages)/(platform)/_components/ZeroContent.jsx
@@ -6,7 +6,7 @@ export default function ZeroContent({ text }) {
 		<div className={"flex-middle flex-col gap-4"}>
 			<div
 				className={"w-25 h-25 bg-[url('/img/pictures/zero-content.png')] bg-no-repeat bg-cover bg-center"}
-				style={"width: 100px;height: 100px;"}></div>
+				style={{ width: "100px", height: "100px" }}></div>
 			{text && <p className={"text-black/40 text-center"}>{text}</p>}
 		</div>
 	);
